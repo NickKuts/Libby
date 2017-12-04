d8e7633d5097650f4d175933c7ee45920e59624b
Alqio <kyosti.alkio@gmail.com>
2017-12-04 17:51:48 +0200

    get_weather.py script now works

--- get_weather.py
+++ get_weather.py
@@ -1,56 +1,55 @@
 import json
 import urllib.request
 from pprint import pprint
+
+from lxml import etree
 import xml.etree.ElementTree as ET
 
-import datetime
+from datetime import datetime, timedelta
 
 # More information from
 # http://lassisavola.net/2016/02/04/fmi-avoin-data-python-ja-pylvasdiagrammit-osa-2/
+# https://docs.python.org/2/library/xml.etree.elementtree.html
 
 apikey = "eb3a4102-7afd-4740-8901-bf6417df01c3"
-place = "otaniemi,Espoo"
-url = "http://data.fmi.fi/fmi-apikey/" + apikey + "/wfs?request=getFeature&storedquery_id=fmi::forecast::hirlam::ground::point::timevaluepair&place=" + place + "/"
-url2 = "http://data.fmi.fi/fmi-apikey/eb3a4102-7afd-4740-8901-bf6417df01c3/wfs?request=getFeature&storedquery_id=fmi::forecast::hirlam::ground::point::timevaluepair&place=jaala/"
 
-date = '2017-12-03'
-time = datetime.datetime.now().isoformat()
+def format_time(time):
+    temp = time[:time.find(':')+3] + 'Z'
+    return temp
 
-time_str = '&starttime=2017-12-04T14:30Z&endtime=2017-12-04T15:50Z'
-query_str = 'fmi::observations::weather::simple&place=otaniemi,espoo'
+
+start_time = (datetime.now() - timedelta(hours=2)).isoformat()
+end_time = (datetime.now() - timedelta(minutes=15)).isoformat()
+
+time_str = '&starttime=' + format_time(start_time) + '&endtime=' + format_time(end_time)
+
+place_str = 'otaniemi,espoo'
+query_str = 'fmi::observations::weather::simple&place=' 
 parameters_str = '&parameters=temperature'
 
-url = "http://data.fmi.fi/fmi-apikey/eb3a4102-7afd-4740-8901-bf6417df01c3/wfs?request=getFeature&storedquery_id=" + query_str + time_str + parameters_str
-
-
-
-
+url = "http://data.fmi.fi/fmi-apikey/"+apikey+"/wfs?request=getFeature&storedquery_id=" + query_str + place_str + time_str  + parameters_str
+print(url)
 datafile_url = "weather_data.xml"
-
 
 
 def read_data():
     tree = ET.parse(datafile_url)
     root = tree.getroot()
-
-    results = tree.findall('{http://www.opengis.net/wfs/2.0}ParameterValue')
-    print(results)
-    print("itse juttu")
     
-    for child in root.findall('{http://www.opengis.net/wfs/2.0}member'):
-        #print(child.tag, child.attrib)
-        for c in child:
-            #print(c.tag, c.attrib)
-            #print(c)
-            for cc in c:
-                testi = "{http://xml.fmi.fi/schema/wfs/2.0}"
-                print(cc.tag)
-                print(cc.get(testi + 'ParameterValue'))
-                print(cc.find('{http://www.opengis.net/wfs/2.0}ParameterValue'))
-                #print(cc.get('temperature'))
+    temperatures = {}
+    
+    for child in root:
+        temp = child[0][3].text
+        time = child[0][1].text
+        print(time, temp)
+        temperatures[time] = float(temp)
+
+    return temperatures
+    
 
 def update_data(web_url, data_url):
     urllib.request.urlretrieve(web_url, data_url)
 
-#update_data(url, datafile_url)
+update_data(url, datafile_url)
 read_data()
+
