76f2f83bb595a869b8d246cf849fc8baa2fbca5b
Alqio <kyosti.alkio@gmail.com>
2017-12-04 17:02:08 +0200

    get_weather.py now prints the elements, but cannot access their attributes yet

--- get_weather.py
+++ get_weather.py
@@ -15,7 +15,15 @@ url2 = "http://data.fmi.fi/fmi-apikey/eb3a4102-7afd-4740-8901-bf6417df01c3/wfs?r
 
 date = '2017-12-03'
 time = datetime.datetime.now().isoformat()
-url = "http://data.fmi.fi/fmi-apikey/eb3a4102-7afd-4740-8901-bf6417df01c3/wfs?request=getFeature&storedquery_id=fmi::observations::weather::simple&place=otaniemi,espoo"
+
+time_str = '&starttime=2017-12-04T14:30Z&endtime=2017-12-04T15:50Z'
+query_str = 'fmi::observations::weather::simple&place=otaniemi,espoo'
+parameters_str = '&parameters=temperature'
+
+url = "http://data.fmi.fi/fmi-apikey/eb3a4102-7afd-4740-8901-bf6417df01c3/wfs?request=getFeature&storedquery_id=" + query_str + time_str + parameters_str
+
+
+
 
 datafile_url = "weather_data.xml"
 
@@ -25,13 +33,24 @@ def read_data():
     tree = ET.parse(datafile_url)
     root = tree.getroot()
 
+    results = tree.findall('{http://www.opengis.net/wfs/2.0}ParameterValue')
+    print(results)
+    print("itse juttu")
+    
     for child in root.findall('{http://www.opengis.net/wfs/2.0}member'):
         #print(child.tag, child.attrib)
         for c in child:
-            print(c.tag, c.attrib)
+            #print(c.tag, c.attrib)
+            #print(c)
+            for cc in c:
+                testi = "{http://xml.fmi.fi/schema/wfs/2.0}"
+                print(cc.tag)
+                print(cc.get(testi + 'ParameterValue'))
+                print(cc.find('{http://www.opengis.net/wfs/2.0}ParameterValue'))
+                #print(cc.get('temperature'))
 
 def update_data(web_url, data_url):
     urllib.request.urlretrieve(web_url, data_url)
 
-update_data(url, datafile_url)
+#update_data(url, datafile_url)
 read_data()
