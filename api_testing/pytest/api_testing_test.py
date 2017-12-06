d1bc6f29a5883a4fd2991b48088300f838ce9d4c
cookie <axel.neergaard@gmail.com>
2017-12-06 21:15:24 +0200

    Adds main function to test case

--- api_testing/pytest/api_testing_test.py
+++ api_testing/pytest/api_testing_test.py
@@ -1,13 +1,13 @@
 import unittest
 import os
+import sys
+sys.path.append('..')
 
-# import main_handler
-from api_testing import main_handler
+import main_handler
 from api_testing import api_testing
-import json
 
 
-class TestUtilDate(unittest.TestCase):
+class TestAPI(unittest.TestCase):
     """ Tests for `api_testing.py` """
 
     json_dir = '../' + api_testing.json_dir
@@ -21,11 +21,20 @@ class TestUtilDate(unittest.TestCase):
             os.remove(os.path.join(self.json_dir, f))
 
     def test_status_code(self):
-        """ Test wether the `do_requeset_json()` returns a 200 response code """
+        """ Test whether the `do_request_json()` returns a 200 response code """
         response = api_testing.do_request_json('')
         self.assertEqual(response['status_code'], 200)
 
     def test_file_creation(self):
-        """ Test wether the function `do_request_file` creates a file """
+        """ Test whether the function `do_request_file` creates a file """
         response = api_testing.do_request_file('something')
         self.assertTrue(response['filename'] in os.listdir(self.json_dir))
+
+
+def main():
+    print("Main function")
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestAPI)
+    unittest.TextTestRunner(verbosity=2).run(suite)
