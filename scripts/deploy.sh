396f70da9ba51d4d4a498165c7fe9e91e925b760
Tuomas <tuomas.j.kontola@aalto.fi>
2018-01-05 14:40:36 +0200

    removed useless scripts folder

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd ../lambda_func
-zip -r ../deploy.zip .
-cd ..
-aws lambda update-function-code \
-    --function-name tuomasTestFunction \
-    --zip-file fileb://deploy.zip
-rm deploy.zip
