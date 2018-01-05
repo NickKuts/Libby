928687b265c2967417d27103528afe77f8335fae
Panula Tomi <panulat1@niksula.hut.fi>
2018-01-05 12:21:21 +0200

    moi lasse

--- lambda_func/util.py
+++ lambda_func/util.py
@@ -1,32 +1,14 @@
 # --------------- Helpers that build all of the responses ----------------------
 
-def build_speechlet_response(title, output, reprompt_text, should_end_session):
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        # "We have classics, lattes, coffee drinks, cappucinos, espressos, hot drinks and cold drinks"
-        },
-        'card': {
-            'type': 'Simple',
-            'title': "SessionSpeechlet - " + title,
-            'content': "SessionSpeechlet - " + output
-        },
-        'reprompt': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': reprompt_text
-            }
-        },
-        'shouldEndSession': should_end_session
-    }
 
-
-def build_response(session_attributes, speechlet_response):
+def close(sessionAttributes, fulfillmentState, message):
     return {
-        'version': '1.0',
-        'sessionAttributes': session_attributes,
-        'response': speechlet_response
+        'sessionAttributes': sessionAttributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillmentState,
+            'message': message
+        }
     }
 
 
@@ -58,12 +40,11 @@ def unhandled_request():
 
 
 def handle_session_end_request():
-    card_title = "Session Ended"
-    speech_output = "Hope you found what you were looking for"
-    # Setting this to true ends the session and exits the skill.
-    should_end_session = True
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
+    message = {
+        'contentType': 'PlainText',
+        'content': "Hope you found what you were looking for"
+    }
+    return close({}, 'Fulfilled', message)
 
 
 """
