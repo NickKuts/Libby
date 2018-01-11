700e089090b70b3cccd83a1be84762d275a3df44
cookie <axel.neergaard@gmail.com>
2018-01-11 12:32:24 +0200

    Added full code coverage for api_testing

--- api_testing/pytest/api_testing_test.py
+++ api_testing/pytest/api_testing_test.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import shutil
 
 from api_testing import api_testing
 
@@ -7,17 +8,39 @@ from api_testing import api_testing
 class TestAPI(unittest.TestCase):
     """ Tests for `api_testing.py` """
 
+    """ For testing purposes we'll use another directory """
+    api_testing.json_dir = './api_testing/data_files_test/'
     json_dir = api_testing.json_dir
 
-    def setUp(self):
-        if not os.path.exists(self.json_dir):
-            os.makedirs(self.json_dir)
+    @classmethod
+    def setUpClass(self):  # pragma: no cover
+        """ Remove the dir if it exists to ensure code coverage """
+        if os.path.exists(self.json_dir):
+            shutil.rmtree(self.json_dir)
 
-    def tearDown(self):
-        """ Ensure that all .json files gets deleted """
-        for f in os.listdir(self.json_dir):
-            if f.endswith('.json'):
-                os.remove(os.path.join(self.json_dir, f))
+    def test_file_creation(self):
+        """ Test whether the function `do_request_file` creates a file """
+        response = api_testing.do_request_file('something')
+        self.assertTrue(os.path.exists(self.json_dir))
+        self.assertTrue(response['filename'] in os.listdir(self.json_dir))
+
+    def test_file_creation_with_prettyprint(self):
+        """ Test whether the function `do_request_file` creates a file with prettyprint """
+        response = api_testing.do_request_file('something', pretty_print='1')
+        self.assertTrue(os.path.exists(self.json_dir))
+        self.assertTrue(response['filename'] in os.listdir(self.json_dir))
+        with open(self.json_dir + response['filename']) as f:
+            content = list(f)
+            self.assertTrue(len(content) > 1)
+
+    def test_file_creation_without_prettyprint(self):
+        """ Test whether the function `do_request_file` creates a file without prettyprint """
+        response = api_testing.do_request_file('something')
+        self.assertTrue(os.path.exists(self.json_dir))
+        self.assertTrue(response['filename'] in os.listdir(self.json_dir))
+        with open(self.json_dir + response['filename']) as f:
+            content = list(f)
+            self.assertTrue(len(content) == 1)
 
     def test_json_dir(self):
         self.assertEqual(self.json_dir, api_testing.json_dir)
@@ -27,13 +50,13 @@ class TestAPI(unittest.TestCase):
         response = api_testing.do_request_json('')
         self.assertEqual(response['status_code'], 200)
 
-    def test_file_creation(self):
-        """ Test whether the function `do_request_file` creates a file """
-        response = api_testing.do_request_file('something')
-        self.assertTrue(response['filename'] in os.listdir(self.json_dir))
-
     def test_remove_json(self):
         """ Test whether `remove_json_files()` deletes files """
         api_testing.remove_json_files()
-        for f in os.listdir(self.json_dir):
-            self.assertFalse(f.endswith('.json'))
+        self.assertTrue(os.listdir(self.json_dir) == [])
+
+    @classmethod
+    @unittest.skipIf(os.path.exists(api_testing.json_dir), "test directory does not exist")
+    def tearDownClass(self):  # pragma: no cover
+        """ Delete the now non-needed directory """
+        shutil.rmtree(self.json_dir)
