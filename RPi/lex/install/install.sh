7418a11e4872861ef52b22ca0bbff985f93e4cce
Kyosti Alkio <kyosti.alkio@gmail.com>
2018-01-15 15:49:37 +0200

    Modified install script

--- RPi/lex/install/install.sh
+++ RPi/lex/install/install.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
-
-
-sudo apt-get update
-sudo apt-get install sox
-sudo apt-get install mpg321
-
-sudo apt-get install python-pyaudio python3-pyaudio
-pip3 install pyaudio
+sudo apt-get install swig3.0 python-pyaudio python3-pyaudio sox
 sudo apt-get install libatlas-base-dev
+pip3 install pyaudio
 pip3 install boto3
+cd
+mkdir .aws 
+cd .aws
+printf "[default]\nregion=eu-west-1" > config
+printf "[default]\naws_access_key_id = YOUR_ACCESS_KEY\naws_secret_access_key = YOUR_SECRET_KEY" > credentials
+
