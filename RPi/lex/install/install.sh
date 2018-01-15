b15685aca581dc05d0ffda9b78a773fdd9cec495
Kyosti Alkio <kyosti.alkio@gmail.com>
2018-01-15 15:39:38 +0200

    Added install script for raspberry pi

---
+++ RPi/lex/install/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+sudo apt-get update
+sudo apt-get install sox
+sudo apt-get install mpg321
+
+sudo apt-get install python-pyaudio python3-pyaudio
+pip3 install pyaudio
+sudo apt-get install libatlas-base-dev
+pip3 install boto3
