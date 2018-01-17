5218516896d689617353bd8e1ea3304c36aaef99
Kyosti Alkio <kyosti.alkio@gmail.com>
2018-01-17 11:14:55 +0200

    Modified install script to automatically install all required things so snowboy code is not needed in the repo :)

--- RPi/lex/install/install.sh
+++ RPi/lex/install/install.sh
@@ -1,7 +1,35 @@
 sudo apt-get install swig3.0 python-pyaudio python3-pyaudio sox
 sudo apt-get install libatlas-base-dev
+sudo apt-get install python-dev
+sudo apt-get install python3-dev
+
 pip3 install pyaudio
 pip3 install boto3
+
+git clone https://github.com/Kitt-AI/snowboy.git
+cd snowboy/swig/Python3
+sed -i '5s/.*/SWIG := swig3.0/' Makefile
+make
+rm Makefile
+mv * ../../../../python/snowboy/
+
+cd ../../examples/Python3
+mv snowboydecoder.py ../../../../python/snowboy/
+# stupid workaround on symbolic links
+cp snowboydetect.py s1.py
+rm snowboydetect.py
+mv s1.py snowboydetect.py
+mv snowboydetect.py ../../../../python/snowboy/
+cp _snowboydetect.so s1.so
+rm _snowboydetect.so
+mv s1.so _snowboydetect.so
+mv _snowboydetect.so ../../../../python/snowboy/
+
+cd ../../..
+
+rm -r snowboy
+
+
 cd
 mkdir .aws 
 cd .aws
