2232ceed20aeeefd4974076ebffb70dc3e584830
cookie <axel.neergaard@gmail.com>
2018-02-05 15:45:31 +0200

    Started creating proper OSM queries

--- OSM/aaltomap-json-test.py
+++ OSM/aaltomap-json-test.py
@@ -1,26 +1,86 @@
+#!/usr/bin/env python3.6
+
+from OSMPythonTools.nominatim import Nominatim
+from OSMPythonTools.overpass import overpassQueryBuilder, Overpass
+from OSMPythonTools.api import Api
 import json
+import requests
 
-filename = 'useful-aaltomap.json'
+
+_osm_api = Api()
 
 
-def test(f_p):
+def get_restaurants(f_p):
+    """ 
+    Function for getting all restaurants from the Useful Aaltomap JSON file. 
+    The function saves both the name of the restaurant, as well as the equivalent OSM node id
+    """
+    restaurants = []
     aalto = json.load(f_p)
+    # The JSON file has (for some reason) separated restaurants into two JSON variables,
+    # thus we have to got through both
     for v in aalto['buildings']:
-        if 'type' in v and v['type'] == 'restaurant':
-            print('\t' + v['type'])
         if 'children' in v:
             child = v['children']
             for c in child:
                 if 'type' in c and c['type'] == 'restaurant':
                     if 'name' in c:
-                        print('name: ' + c['name'])
+                        restaurants.append((c['name'], c['osm'].replace("=", "/")))
                     else:
-                        print('id: ' + c['id'])
-    print('###' * 30)
+                        restaurants.append((c['id'].title(), c['osm'].replace("=", "/")))
     for v in aalto['other']:
         if 'type' in v and v['type'] == 'restaurant':
-            print('name: ' + v['name'])
+            restaurants.append((v['name'], v['osm'].replace("=", "/")))
+    return restaurants
 
 
-with open(filename, 'r') as f:
-    test(f)
+def get_rest_location(rest):
+    """
+    Function for getting tags related to a certain restaurant
+    :param rest: the restaurant node
+    :return: associated tags
+    """
+    if 'node=' in rest:
+        rest.replace("=", "/")  # The string format has to be 'node/<id>'
+    elif 'node' not in rest:
+        rest = 'node/' + rest
+    return _osm_api.query(rest).tags()
+
+
+def otaniemi():
+    xml = """
+    <query type="node">
+        <has-kv k="name" v="Otaniemi"/>
+    </query>
+    <around radius="300"/>
+    <print/>
+    """
+    headers = {'Content-Type': 'application/xml'}
+    query = requests.post(
+        'https://lz4.overpass-api.de/api/interpreter',
+        data=xml,
+        headers=headers
+    )
+    print(query.text)
+
+
+def aaltomap_handler(event, context):
+    """ Handler function for the AWS Lambda function """
+    filename = event['filename']
+
+    with open(filename, 'r') as f:
+        restaurants = get_restaurants(f)
+
+    get_rest_location(restaurants[0][1])
+
+    return {
+        'sessionAttributes': {},
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': 'Fulfilled',
+            'message': {
+                'content': restaurants,
+            }
+        }
+    }
+
