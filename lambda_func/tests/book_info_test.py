6f06c8fe8db58200b1f1271e183c17b79c0242e8
Sofia Kimpimäki <kimpims@aalto.fi>
2018-02-09 16:37:34 +0200

    added some functionalities

--- lambda_func/tests/book_info_test.py
+++ lambda_func/tests/book_info_test.py
@@ -2,9 +2,20 @@ import unittest
 import json
 from lambda_func import main_handler, book_info
 
+test_data = json.load(open("./tests/help_test.json"))
 
-class TestRoberts(unittest.TestCase):
+
+
+class TestBookInfo(unittest.TestCase):
     test_data = json.load(open("tests/book_info_test.json"))
+
+    def test_book_info(self):
+        books = ['findComputer']
+        for i in books:
+            testStr = self.test_data[i]
+            result = main_handler.lambda_handler(testStr, None)
+            print("result", result)
+            assert (1==1)
 
 
 def main():
@@ -12,5 +23,5 @@ def main():
 
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestRoberts)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestBookInfo)
     unittest.TextTestRunner(verbosity=2).run(suite)
