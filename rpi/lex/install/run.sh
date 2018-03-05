30172af7661450359321397ad3b2894e16d65fc4
Tuomas <tuomas.j.kontola@aalto.fi>
2018-03-05 15:18:40 +0200

    Removed AWS keys from file. Key has been deactivated

--- rpi/lex/install/run.sh
+++ rpi/lex/install/run.sh
@@ -1,7 +1,4 @@
 docker run\
-    -e AWS_DEFAULT_REGION=eu-west-1 \
-    -e AWS_ACCESS_KEY_ID=AKIAJSY4GPTL3VRDR3NQ \
-    -e AWS_SECRET_ACCESS_KEY=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 \
     -e PCM_DEVICE='sysdefault:CARD=USB' \
     --device /dev/snd --privileged \
     libbypi:latest run
