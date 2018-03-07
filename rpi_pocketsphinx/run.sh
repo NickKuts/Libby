b8f204c30a404c091184a2d0b4d6b3120e113095
Kyosti Alkio <kyosti.alkio@gmail.com>
2018-03-07 14:37:21 +0200

    Removed api keys from git and modified gitignore

--- rpi_pocketsphinx/run.sh
+++
@@ -1,7 +0,0 @@
-docker run \
-	-e AWS_DEFAULT_REGION=eu-west-1 \
-	-e AWS_ACCESS_KEY_ID=AKIAJLMUJKSJ4ZBYZHEQ \
-	-e AWS_SECRET_ACCESS_KEY=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 \
-	-e PCM_DEVICE='sysdefault:CARD=USB' \
-	--device /dev/snd --privileged \
-	libbypi run
