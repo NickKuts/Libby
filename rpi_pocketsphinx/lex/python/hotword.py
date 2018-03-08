62e3b8ff63dd22305f2a142d767fc4f38e89353f
Tuomas <tuomas.j.kontola@aalto.fi>
2018-03-08 15:42:26 +0200

    Added extra variations of libby hotword

--- rpi_pocketsphinx/lex/python/hotword.py
+++ rpi_pocketsphinx/lex/python/hotword.py
@@ -3,23 +3,25 @@ import pyaudio
 from pocketsphinx.pocketsphinx import *
 from sphinxbase.sphinxbase import *
 
-MODELDIR = "/rpi/lex/python/model/"
+MODELDIR = "model/"
+PATHDIR = "/rpi/lex/python/"
 p = pyaudio.PyAudio()
 # Create a decoder with certain model
 config = Decoder.default_config()
 config.set_string('-hmm', path.join(MODELDIR, 'en-us/'))
-config.set_string('-lm', '/rpi/lex/python/3268.lm')
-config.set_string('-dict', '/rpi/lex/python/3268.dic')
-config.set_float('-kws_threshold',  1e-15)
-config.set_string('-logfn', '/dev/null')
+config.set_string('-dict', path.join(PATHDIR, '0963.dic'))
+# config.set_string('-logfn', '/dev/null')
 decoder = Decoder(config)
-decoder.set_keyphrase("kws", "LIBBY")
+
+decoder.set_kws("kws", path.join(PATHDIR, 'keywords.txt'))
+decoder.set_lm_file("lm", path.join(PATHDIR, '0963.lm'))
 decoder.set_search("kws")
+
 
 def start(callback):
     # Decode streaming data.
     decoder.start_utt()
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=48000,
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000,
                     input=True, input_device_index=None,
                     frames_per_buffer=20480)
     stream.start_stream()
