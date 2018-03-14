85b923845e74e58516c13b7c02d4ab726c2409c8
cookie <axel.neergaard@gmail.com>
2018-03-14 22:32:15 +0200

    Small changes to script

--- OSM/get_sample_utterances.py
+++ OSM/get_sample_utterances.py
@@ -5,6 +5,13 @@ import sys
 import json
 
 
+### Note: this script will overwrite any file named 'sample-utterances.json'
+###       in the directory of this script
+json_name = 'sample_utterances.json'
+
+# Get absolute path to the directory where this file is
+ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+
 # Get possible arguments from user
 argums = sys.argv
 
@@ -16,8 +23,8 @@ else: intent = input('intent> ')
 
 # Now do the query, the result will be a string so we 
 # have to change it later on
-#res_str = os.system("aws lex-models get-intent --name {} --intent-version \$LATEST".format(intent))
-res_str = os.popen("aws lex-models get-intent --name {} --intent-version \$LATEST".format(intent)).read()
+res_str = os.popen("aws lex-models get-intent --name "
+                   "{} --intent-version \$LATEST".format(intent)).read()
 
 # Check if errors occurred
 if 'error occurred' in res_str:
@@ -33,12 +40,13 @@ in_json = in_json['sampleUtterances']
 # if not, do not indent (i.e. pretty-print or not)
 try:
     indent = int(argums[2])
-except: indent = None
+except (ValueError, IndexError): 
+    indent = None
 
 # Maybe check if the file already exists?
 
 # And finally write the JSON file depending on the indentation
-with open('sample-utterances.json', 'w+') as fp:
+with open(ABS_PATH + '/' + json_name, 'w+') as fp:
     if indent:
         json.dump(in_json, fp, indent=indent)
     else: json.dump(in_json, fp)
