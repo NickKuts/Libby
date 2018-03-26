97b37016831f5f06b7538394b8ebd95d0deeeb40
Tuomas Kontola <tuomas.j.kontola@aalto.fi>
2018-03-26 12:33:07 +0000

    Fixed another typo

--- rpi_pocketsphinx/lex/install/run.sh
+++ rpi_pocketsphinx/lex/install/run.sh
@@ -1,8 +1,7 @@
 docker run\
-    -e AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} \
-    -e AWS_ACCES_KEY_ID=${AWS_ACCES_KEY_ID} \
-    -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+    -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     -e PCM_DEVICE='sysdefault:CARD=USB' \
     --device /dev/snd \
-    --name=PROJECT_LIBBY \
     libbypi:latest run
