c6156f5c07a044f756fef2baa87c1ff2adb809fd
Tuomas <tuomas.j.kontola@aalto.fi>
2018-04-04 11:07:39 +0300

    Added working install script

--- rpi_pocketsphinx/lex/install/install.sh
+++ rpi_pocketsphinx/lex/install/install.sh
@@ -2,6 +2,15 @@ curl -sSL https://get.docker.com | sh
 cd ../../
 make
 cd lex/install/
+script="docker run\
+    -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    -e PCM_DEVICE='sysdefault:CARD=USB' \
+    --device /dev/snd --privileged\
+    libbypi:latest run"
+touch run.sh
+echo $script > run.sh
 bash run.sh
 cp docker-Libby.service /etc/systemd/system/
 systemctl enable docker-Libby.service
--- rpi_pocketsphinx/lex/install/run.sh
+++
@@ -1,7 +0,0 @@
-docker run\
-    -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
-    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-    -e PCM_DEVICE='sysdefault:CARD=USB' \
-    --device /dev/snd --privileged\
-    libbypi:latest run
