5ffa7f8a076af9632cd67e30920ad430df25429a
Sofia Kimpimäki <kimpims@aalto.fi>
2018-04-06 18:27:06 +0300

    fixed invalid lambda response

--- lambda_func/get_help.py
+++ lambda_func/get_help.py
@@ -2,9 +2,9 @@ import util
 import json
 
 """
-help_answer return the help answer depend which help is asking. If user use
+help_answer return the help which help user is asking. If user use
 only word "help" the answer is the basic Libby answer other way the answer is
-from the "help.json" -file. Where is all the help which are useful.
+from the "help.json" -file. Where is all the help which are useful. 
 """
 
 
@@ -16,6 +16,13 @@ def help_answer(intent):
         return util.elicit_intent({}, message)
     else:
         data = json.load(open('help.json'))
-        name = intent['slots']['what']
-        message = data[name]
+        what_name = intent['slots']['what']
+        if what_name in data[what_name]:
+                message = data[what_name]
+        else:
+            message = "I'm sorry, I didn't found" + what_name + "help. " \
+                    "You can ask me about the weather, roberts coffee menu, " \
+                    "locations or more information about some book or author"
+
         return util.elicit_intent({}, message)
+
